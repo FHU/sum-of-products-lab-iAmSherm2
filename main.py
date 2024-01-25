2a48233cad98e1676cf86ca7cb361522b2c781ef
iAmSherm2 <trace.anderson@students.fhu.edu>
2024-01-25 10:01:23 -0600

    updated main.py
    Added if statement with a return error and not sure what the second part is

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #REMOVE PASS AND FIX THE FUNCTION
 def sum_of_products(list1, list2):
-    pass
+    if len(list1) != len(list2):
+        return 'Error'
 
 if __name__ == '__main__':
    #REMOVE PASS AND YOUR CODE GOES HERE
-    pass
+    
