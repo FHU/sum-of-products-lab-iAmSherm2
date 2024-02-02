f60d2098a281fe10a2e71fc73e16e7c152c2b722
iAmSherm2 <trace.anderson@students.fhu.edu>
2024-02-01 21:49:24 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 #REMOVE PASS AND FIX THE FUNCTION
 def sum_of_products(list1, list2):
     if len(list1) != len(list2):
-        return 'Error'
+        raise ValueError
     result = sum(x * y for x, y in zip(list1, list2))
     return result
 
 
-if __name__== '__main__':
+if __name__ == '__main__':
     list1 = list(map(int, input()))
     list2 = list(map(int, input()))
     output = sum_of_products(list1, list2)
